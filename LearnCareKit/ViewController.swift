8fc7ba97f593f719d82909a80d1d9f6030bf8fba
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-05-03 14:32:21 -0400

    Intialize and display a care card

--- LearnCareKit/ViewController.swift
+++ LearnCareKit/ViewController.swift
@@ -3,7 +3,28 @@ import CareKit
 
 class ViewController: UIViewController {
 
+    private let storeDirectory: NSURL =  {
+        let searchPaths = NSSearchPathForDirectoriesInDomains(.ApplicationSupportDirectory, .UserDomainMask, true)
+        let applicationSupportPath = searchPaths[0]
+        let persistenceDirectoryURL = NSURL(fileURLWithPath: applicationSupportPath)
+
+        if !NSFileManager.defaultManager().fileExistsAtPath(persistenceDirectoryURL.absoluteString, isDirectory: nil) {
+            try! NSFileManager.defaultManager().createDirectoryAtURL(persistenceDirectoryURL, withIntermediateDirectories: true, attributes: nil)
+        }
+
+        return persistenceDirectoryURL
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let store = OCKCarePlanStore(persistenceDirectoryURL: storeDirectory)
+        let careVC = OCKCareCardViewController(carePlanStore: store)
+
+        presentViewController(careVC, animated: true, completion: nil)
     }
 }
