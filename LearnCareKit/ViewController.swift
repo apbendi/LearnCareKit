5a9a457133e96f42f187fb6ccd7bff1919661be9
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-05-03 15:18:38 -0400

    Add an intervention activity to the care card

--- LearnCareKit/ViewController.swift
+++ LearnCareKit/ViewController.swift
@@ -23,6 +23,30 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
 
         let store = OCKCarePlanStore(persistenceDirectoryURL: storeDirectory)
+
+        let components = NSCalendar.currentCalendar().componentsInTimeZone(NSTimeZone.defaultTimeZone(), fromDate: NSDate())
+        let schedule = OCKCareSchedule.dailyScheduleWithStartDate(components, occurrencesPerDay: 1)
+
+        let intActivity = OCKCarePlanActivity.interventionWithIdentifier("InterventionActivity",
+                                                                      groupIdentifier: nil,
+                                                                      title: "First Intervention",
+                                                                      text: nil,
+                                                                      tintColor: nil,
+                                                                      instructions: nil,
+                                                                      imageURL: nil,
+                                                                      schedule: schedule,
+                                                                      userInfo: nil)
+        store.addActivity(intActivity) { (didAdd, error) in
+            if didAdd {
+                print("Added intervention activity")
+            } else if let error = error {
+                print("Error adding activity: \(error.localizedDescription)")
+            } else {
+                print("Did not at intervention activity")
+            }
+        }
+
+
         let careVC = OCKCareCardViewController(carePlanStore: store)
 
         presentViewController(careVC, animated: true, completion: nil)
